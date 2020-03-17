489d4b429a5155ac993b37bfa55cc81c63f45db3
Jonathan Thornander <43991450+JonathanThornander@users.noreply.github.com>
2020-03-17 17:52:25 +0100

    Now Faster With MultiProcessing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import multiprocessing
+
 import visualizer
 from experiment_runner import run
 
@@ -10,28 +12,31 @@ from algorithms.mergesort import Mergesort
 from algorithms.bogosort import Bogosort
 from datagenerators import *
 
+
+# A list containing datagenerators, used to generate arrays of integers (i.e sorted, almost sorted, etc)
+datagenerators = [UnsortedData, SortedData, AlmostSortedData, ReversedData]
+# A list containing algorithms
+algorithms = [QuickSort, SelectionSort, Insertionsort]
+maxsize = 400  # Maximum list size. Should be lower than 2000 when using recursive algorithms
+repeat = 40  # Controlls how many list that should be generated for a given lenght and type. Low value = Hight Accuracy
+step = 40  # Controlls how much the list size will vary for every iteration. Low value = Hight Accuracy
+
+
 def main():
-
-    # A list containing datagenerators, used to generate arrays of integers (i.e sorted, almost sorted, etc)
-    datagenerators = [UnsortedData, SortedData, AlmostSortedData, ReversedData]
-    # A list containing algorithms
-    algorithms = [QuickSort, SelectionSort, Insertionsort]
-    maxsize = 400  # Maximum list size. Should be lower than 2000 when using recursive algorithms
-    repeat = 40  # Controlls how many list that should be generated for a given lenght and type. Low value = Hight Accuracy
-    step = 40  # Controlls how much the list size will vary for every iteration. Low value = Hight Accuracy
-
-    loadindicator = 0 # Used to display load indicator for user
     print("Starting...")
+    processes = []
     for datagenerator in datagenerators:
-        print(f"Colleting data from {datagenerator.name}")
+        p = multiprocessing.Process(target=execute, args=(datagenerator,))
+        p.start()
+        processes.append(p)
 
-        # Get data from test with
-        results = run(algorithms=algorithms, datagenerator=datagenerator, maxsize=maxsize, repeat=repeat, step=step)
-        visualizer.plot_results(results, datagenerator.name)
+    for process in processes:
+        process.join()
 
-        loadindicator += 1/len(datagenerators)
-        print(f"{int(loadindicator*100)} % done...")
-
+def execute(datagenerator):
+    results = run(algorithms=algorithms, datagenerator=datagenerator, maxsize=maxsize, repeat=repeat, step=step)
+    visualizer.plot_results(results, datagenerator.name)
+    print(f"Done calculating {datagenerator.name}")
     visualizer.show()
     
 
