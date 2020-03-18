8d31b124bfa1757ffdf0a15e9104a7f0e3affc25
Christoffer Ivarsson Orrelid <58771651+ToffeIO@users.noreply.github.com>
2020-03-18 23:51:16 +0100

    Update cocktailsort.py

--- algorithms/cocktailsort.py
+++ algorithms/cocktailsort.py
@@ -1,3 +1,6 @@
+# Works in the same way as bubblesort, with the addition of moving elements both up the index aswell as down. 
+# This makes it the same complexity as bubblesort, but slightly faster on average.
+
 class Cocktailsort:
     name = "Coctail-Sort"
 
@@ -10,41 +13,26 @@ class Cocktailsort:
         start = 0
         end = n-1
         while (swapped==True): 
-    
-            # reset the swapped flag on entering the loop, 
-            # because it might be true from a previous 
-            # iteration. 
+   
             swapped = False
-    
-            # loop from left to right same as the bubble 
-            # sort 
+     
             for i in range (start, end): 
                 self.n_ops += 1
                 if (array[i] > array[i+1]) : 
                     array[i], array[i+1]= array[i+1], array[i] 
                     swapped=True
     
-            # if nothing moved, then array is sorted. 
             if (swapped==False): 
                 break
     
-            # otherwise, reset the swapped flag so that it 
-            # can be used in the next stage 
             swapped = False
     
-            # move the end point back by one, because 
-            # item at the end is in its rightful spot 
             end = end-1
     
-            # from right to left, doing the same 
-            # comparison as in the previous stage
             self.n_ops += 1
             for i in range(end-1, start-1,-1): 
                 if (array[i] > array[i+1]): 
                     array[i], array[i+1] = array[i+1], array[i] 
                     swapped = True
     
-            # increase the starting point, because 
-            # the last stage would have moved the next 
-            # smallest number to its rightful spot. 
             start = start+1
