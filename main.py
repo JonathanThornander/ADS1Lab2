25baf8948c439cbd570f6bd83d77f1dbf5677a34
Christoffer Ivarsson Orrelid <58771651+ToffeIO@users.noreply.github.com>
2020-03-19 00:07:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from datagenerators import *
 
 datagenerators = [UnsortedData, SortedData, AlmostSortedData, ReversedData] # A list containing datagenerators, used to generate arrays of integers (i.e sorted, almost sorted, etc)
 algorithms = [QuickSort, SelectionSort, Insertionsort] # A list containing algorithms
-maxsize = 100  # Maximum list size. Should be lower than 2000 when using recursive algorithms
+maxsize = 100  # Maximum list size. Can be hard on memory if to high when using recursive algorithms
 repeat = 20  # Decides how many lists that should be generated for a given lenght and type, and gives the average. High value = High Accuracy
 step = 10  # Decides how much the list size will vary for every iteration. Low value = High Accuracy
 
